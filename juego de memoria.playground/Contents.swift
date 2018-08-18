7659de574726da1fbc91e4c17cc81c0ea87e73b8
Daniel Rocha <user143257@74-80-245-37.ic2net.net>
2018-08-18 09:38:53 -0700

    Primera version del juego memoria
    Queda chilo

---
+++ juego de memoria.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var rango = 0...100
+
+for numero in rango{
+    
+    if numero % 5 == 0 {
+        print("# \(numero), Bingo")
+    }
+    
+    if numero % 2 == 0 {
+        print("# \(numero), par")
+    } else {
+        print("# \(numero), impar")
+    }
+    
+    if numero >= 30 && numero <= 40 {
+        print("# \(numero), Viva Swift!!!")
+    }
+}
